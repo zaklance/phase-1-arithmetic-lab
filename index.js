1c2c7253ddf39194bf5c2a805ece9944ecc1efcd
zaklance <wosewick@gmail.com>
2024-04-20 13:35:23 -0400

    done

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+const num1 = 2;
+const num2 = 31;
+const num3 = 16;
+const num4 = 6;
 
-// Write your code here
+const multiply = num1 * num2;
+
+// function multiply(){
+//     return num1 * num2;
+// }
+
+const random = (Math.ceil(Math.random() * 10));
+
+const mod = (num3%num4);
+
+const max = Math.max(2, 4, 20);
+
